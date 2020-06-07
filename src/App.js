519e19b6be3e0e4365dcc478b37a9fd51723b0a2
Austin George <ageorge8000@gmail.com>
2020-06-07 00:09:33 -0400

    Updated Content & Added Images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './sb-blue.jpg';
+import wave from './white sound wave.jpg';
+import slant from './slant.png';
+import ombre from './ombre.png';
 import './App.css';
+
+//Fonts
+import './fonts/Rubik/Rubik-Bold.ttf';
+import './fonts/Rubik/Rubik-Regular.ttf';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" />
+        <h1 className="title">Sound Bound</h1>
+        <p>Restore decency to the audio experience on the internet</p>
+
       </header>
+      <img src={wave} className="transition"/>
+      <section className="App-section">
+        <h1>Welcome to sound reimagined.</h1>
+        <p>We get it. You're tired of the egregiously loud jumpscares and Spotify Ads. Yet, at the same time, you can barely hear the dialogue when you need it most while watching a movie on Netflix. Sound Bound is here to change that.</p>
+      </section>
     </div>
   );
 }
